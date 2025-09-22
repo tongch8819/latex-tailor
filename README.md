9f1b5ab1a4b4385f81c7a930b9a5b649712ece63
tongch8819 <tong.cheng.8819@outlook.com>
2025-09-22 09:42:18 +0800

    add algorithm convertor;

---
+++ README.md
@@ -0,0 +1,11 @@
+# latextailor README
+
+This extension converts LaTeX `multline*` environments to `equation` environments by removing `&` and `\\` symbols.
+
+## Usage
+
+1. Select the LaTeX content you want to convert.
+2. Open the Command Palette (`Ctrl+Shift+P`).
+3. Run the `Convert LaTeX` command.
+
+
---
+++ src/extension.ts
@@ -0,0 +1,76 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    const registerCommand = (command: string, callback: (text: string) => string) => {
+        let disposable = vscode.commands.registerCommand(command, () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+                const selection = editor.selection;
+                const text = editor.document.getText(selection);
+                const convertedText = callback(text);
+
+                editor.edit(editBuilder => {
+                    editBuilder.replace(selection, convertedText);
+                });
+            }
+        });
+        context.subscriptions.push(disposable);
+    };
+
+    registerCommand('extension.convertLatexMultiline', convertLatexMultiline);
+    registerCommand('extension.convertLatexAlgoU2L', convertLatexAlgoU2L);
+    registerCommand('extension.convertLatexAlgoL2U', convertLatexAlgoL2U);
+}
+
+function convertLatexMultiline(content: string): string {
+    content = content.replace(/\\begin{multline\*}/g, '\\begin{equation*}');
+    content = content.replace(/\\end{multline\*}/g, '\\end{equation*}');
+    content = content.replace(/\\begin{multline}/g, '\\begin{equation}');
+    content = content.replace(/\\end{multline}/g, '\\end{equation}');
+    content = content.replace(/\\begin{align\*}/g, '\\begin{equation}');
+    content = content.replace(/\\end{align\*}/g, '\\end{equation}');
+    content = content.replace(/\\begin{align}/g, '\\begin{equation}');
+    content = content.replace(/\\end{align}/g, '\\end{equation}');
+    content = content.replace(/&/g, '');
+    content = content.replace(/\\\\/g, '');
+    content = content.replace(/\\quad/g, '');
+    return content;
+}
+
+function replaceContent(content: string, replacements: { [key: string]: string }): string {
+    for (const [key, value] of Object.entries(replacements)) {
+        content = content.replace(new RegExp(key, 'g'), value);
+    }
+    return content;
+}
+
+function convertLatexAlgoU2L(content: string): string {
+    const replacements = {
+        '\\\\STATE': '\\State',
+        '\\\\WHILE': '\\While',
+        '\\\\ENDWHILE': '\\EndWhile',
+        '\\\\IF': '\\If',
+        '\\\\ENDIF': '\\EndIf',
+        '\\\\FOR': '\\For',
+        '\\\\ENDFOR': '\\EndFor',
+        '\\\\textbf{return}': '\\Return'
+    };
+    return replaceContent(content, replacements);
+}
+
+function convertLatexAlgoL2U(content: string): string {
+    const replacements = {
+        '\\\\State': '\\STATE',
+        '\\\\While': '\\WHILE',
+        '\\\\EndWhile': '\\ENDWHILE',
+        '\\\\If': '\\IF',
+        '\\\\EndIf': '\\ENDIF',
+        '\\\\For': '\\FOR',
+        '\\\\EndFor': '\\ENDFOR',
+        '\\\\Else': '\\ELSE',
+        '\\\\Return': '\\textbf{return}'
+    };
+    return replaceContent(content, replacements);
+}
+
+export function deactivate() {}
